ddb6f67182572e3358e281bb3d0cc363037aca92
Alyssa <46027209+latinawhocodes@users.noreply.github.com>
2022-11-02 08:09:43 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  fetchData = (evt) => {
+    evt.preventDefault();
+    console.log('fetch weather data!');
+  }
+
   render() {
     return (
       <div>
         <h1>Weather</h1>
-        <form>
+        <form onSubmit={this.fetchData}>
           <label>I want to know the weather for
             <input placeholder={"City, Country"} type="text" />
           </label>
