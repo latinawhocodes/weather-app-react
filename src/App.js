7ab8bf3b0ea9787d722bb6dca45829008a3fc0c3
Alyssa <46027209+latinawhocodes@users.noreply.github.com>
2022-11-02 08:17:17 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+      location: ''
+  };
+  
   fetchData = (evt) => {
     evt.preventDefault();
-    console.log('fetch weather data!');
-  }
+    let location = encodeURIComponent(this.state.location);
+
+    let apiKey = '39077aa52648b860160251e27d399aa7';
+    let url = 'http://api.openweathermap.org/data/2.5/forecast?q=' + '&APPID=' + apiKey + '&units=metric';
+  };
+
+  changeLocation = (evt) => {
+    this.setState({
+      location: evt.target.value
+    });
+  };
 
   render() {
     return (
@@ -13,7 +26,12 @@ class App extends React.Component {
         <h1>Weather</h1>
         <form onSubmit={this.fetchData}>
           <label>I want to know the weather for
-            <input placeholder={"City, Country"} type="text" />
+            <input
+              placeholder={"City, Country"}
+              type="text"
+              value={this.state.location}
+              onChange={this.changeLocation}
+            />
           </label>
         </form>
       </div>
